8b56e0ac8833374657c5a813b479d80b717ff5a1
Victor Monteiro <contato@ovictormonteiro.com>
2022-03-12 06:12:25 -0300

    mudanças feitas

--- src/components/homePage/index.tsx
+++ src/components/homePage/index.tsx
@@ -1,64 +1,67 @@
-import { useContext, useEffect, useState } from 'react';
-import {Api} from '../../api'
-import { Context } from '../../context';
-import {PartyTypes, PartyUserTypes} from '../../types'
-import { Imagem } from '../Imagem';
-import { MainGrid } from './Style';
+import { useContext, useEffect, useState } from "react";
+import { Api } from "../../api";
+import { Context } from "../../context";
+import { PartyTypes, PartyUserTypes } from "../../types";
+import { Imagem } from "../Imagem";
+import { MainGrid } from "./Style";
 
-export const HomePage = ()=>{
+export const HomePage = () => {
   let token: string = localStorage.token;
-  const {state} = useContext(Context)
-  const [dataApi, setDataApi] = useState<PartyUserTypes>() 
+  const { state } = useContext(Context);
+  const [dataApi, setDataApi] = useState<PartyUserTypes>();
   let parties: PartyTypes[] = [];
-  let endpoint = state.isLogged ? '/user/parties' : '/parties';
+  let endpoint = state.isLogged ? "/user/parties" : "/parties";
 
-  useEffect(()=>{
+  useEffect(() => {
     apiGet();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[state.isLogged])
-  
-  async function apiGet(){
-    const {json} = await Api.get(endpoint, token && token);
-    if(json.message) return null;
-    if(state.isLogged){
-      setDataApi(json)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.isLogged]);
+
+  async function apiGet() {
+    const { json } = await Api.get(endpoint, token && token);
+    if (json.message) return null;
+    if (state.isLogged) {
+      setDataApi(json);
     } else {
       setDataApi({
-        parties: json
-      })
+        parties: json,
+      });
     }
   }
 
-
-  (function adicionaFesta(){
-    if(dataApi !== undefined){
-      dataApi.parties.forEach((party)=>{
-        parties.push(party)
-      })
-      if(dataApi.privateParties){
-        dataApi.privateParties.forEach((party)=>{
-          parties.push(party)
-        })
+  (function adicionaFesta() {
+    if (dataApi !== undefined) {
+      dataApi.parties.forEach((party) => {
+        parties.push(party);
+      });
+      if (dataApi.privateParties) {
+        dataApi.privateParties.forEach((party) => {
+          parties.push(party);
+        });
       }
     }
-  })()
-  
+  })();
+
   return (
-    <MainGrid className='leftIn'>
-      {parties.length > 0 && <>
-        {parties.map((item)=>{
-          let date = new Date(item.date).toLocaleDateString('pt-BR');
-          return (
-            <div key={item._id}>
-              <Imagem src={`https://avatars.githubusercontent.com/u/84861666?s=400&u=1980f4667d52b84816adf47f73fea4cc35f1cd94&v=4`}/>
-              <h1>{item.title}</h1>
-              <h3>{item.description}</h3>
-              <p>{date}</p>
-            </div>
-          )
-        })}
-      </>} 
-     {parties.length === 0 && <h1>Nenhum evento cadastrado</h1>}
+    <MainGrid className="leftIn">
+      {parties.length > 0 && (
+        <>
+          {parties.map((item) => {
+            let date = new Date(item.date).toLocaleDateString("pt-BR");
+            return (
+              <div key={item._id}>
+                <Imagem
+                  src={`http://localhost:4000/assets/media/${item.photos[0]}`}
+                />
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <p>{date}</p>
+              </div>
+            );
+          })}
+        </>
+      )}
+      {parties.length === 0 && <h1>Nenhum evento cadastrado</h1>}
     </MainGrid>
-  )
-}
+  );
+};
--- src/pages/User/Feed.tsx
+++ src/pages/User/Feed.tsx
@@ -6,35 +6,38 @@ import { PartyTypes } from "../../types";
 import { FeedGrid } from "./Style";
 
 export const Feed = () => {
-  const [temFestas, setTemFestas] = useState<PartyTypes[]>([])
-  const token = localStorage.getItem('token');
-  useEffect(()=>{
+  const [temFestas, setTemFestas] = useState<PartyTypes[]>([]);
+  const token = localStorage.getItem("token");
+  useEffect(() => {
     pegarFestas();
-  },[]) 
-    async function pegarFestas(){
-    const res = await Api.get('/user/parties', token?.toString());
-    let festas:PartyTypes[] = [];
+  }, []);
+  async function pegarFestas() {
+    const res = await Api.get("/user/parties", token?.toString());
+    let festas: PartyTypes[] = [];
     res.json.parties.forEach((festa: PartyTypes) => {
       festas.push(festa);
-    })
+    });
     res.json.privateParties?.forEach((festa: PartyTypes) => {
       festas.push(festa);
-    })
+    });
     setTemFestas(festas);
   }
   return (
     <FeedGrid className="leftIn">
-      {temFestas.length > 0 && temFestas.map((festa: PartyTypes) => {
-        let data = new Date(festa.date).toLocaleDateString('pt-BR');
-        return(
-          <Link key={festa._id} to={`/party/${festa._id}`}>
-            <Imagem src={festa.photos[0]}/>
-            <h1>{festa.title}</h1>
-            <h3>{festa.description}</h3>
-            <p>{data}</p>
-          </Link>
-        )
-      })}
+      {temFestas.length > 0 &&
+        temFestas.map((festa: PartyTypes) => {
+          let data = new Date(festa.date).toLocaleDateString("pt-BR");
+          return (
+            <Link key={festa._id} to={`/party/${festa._id}`}>
+              <Imagem
+                src={`http://localhost:4000/assets/media/${festa.photos[0]}`}
+              />
+              <h2>{festa.title}</h2>
+              <h3>{festa.description}</h3>
+              <p>{data}</p>
+            </Link>
+          );
+        })}
     </FeedGrid>
-  )
-}
+  );
+};
